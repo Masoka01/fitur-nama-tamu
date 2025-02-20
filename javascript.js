821190a94a473f39f753c53eeab5c8ec929236b0
Dimas Mayoni <143168338+Masoka01@users.noreply.github.com>
2025-02-20 08:35:35 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -125,31 +125,3 @@ function showNotification(message) {
 document
   .getElementById("copyButton")
   .addEventListener("click", copyToClipboard);
-
-function createHeart() {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  document.body.appendChild(heart);
-
-  const randomLeft = Math.random() * window.innerWidth;
-  const randomDuration = Math.random() * 3 + 2;
-  const randomColor = `hsl(${Math.random() * 360}, 100%, 60%)`;
-  const randomX = Math.random() * 200 - 100;
-  const randomSize = Math.random() * 20 + 30; // Ukuran acak
-
-  heart.style.left = `${randomLeft}px`;
-  heart.style.animationDuration = `${randomDuration}s`;
-  heart.style.setProperty("--random-x", `${randomX}px`);
-  heart.style.width = `${randomSize}px`;
-  heart.style.height = `${randomSize}px`;
-
-  heart.style.backgroundColor = randomColor;
-  heart.style.boxShadow = `0 0 15px 5px ${randomColor}`;
-  heart.style.filter = `drop-shadow(0 0 10px ${randomColor})`;
-
-  setTimeout(() => {
-    heart.remove();
-  }, randomDuration * 1000);
-}
-
-setInterval(createHeart, 150);
